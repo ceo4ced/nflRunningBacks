c867dadd69485db516097556a46040155d0035fd
Cedric Williams <me@cedricwilliams.com>
2019-12-09 20:14:44 -0500

    Getting Closer

--- nflRunningBackStyles.R
+++ nflRunningBackStyles.R
@@ -1,6 +1,9 @@
 require(dplyr)
 require(ggplot2)
 require(corrplot)
+require(caret)
+require(factoextra)
+require(rpart)
 setwd("~/Documents/GitHub/nflRunningBacks")
 nfl.data <- read.csv("data/train.csv",stringsAsFactors = FALSE)
 
@@ -9,12 +12,21 @@ data <- nfl.data[,c(12,15,16,19,20,21,25,26,27,28,29,30,31,32,33,34,35,37,44,2,1
 
 data.rb <- data[data$Position=="RB",]
 
+
+
 ## reorder columns
 
  data.rb <- data.rb[, c(22,1,15,16,17,18,21,20,19,3,4,6,2,5,7,8,9,10,11,12,13,14)]
 
+ data.rb <- data.rb %>%
+    group_by(NflId) %>%
+    arrange(PlayId) %>%
+    mutate(prePlayYards=lag(Yards),
+           cumYards=cumsum(Yards))
+ 
+ 
  rb.attr <- data.rb[,c(1:6)]
- rb.gm.data <- data.rb[,c(1,7:22)]
+ rb.gm.data <- data.rb[,c(1,7:24)]
  
  splitTime <- function(x){
    
@@ -45,45 +57,73 @@ data.rb <- data[data$Position=="RB",]
  rb.gm.data <- rb.gm.data[rb.gm.data$handoffTime>=0,]
 
  ## Cleaning - removing N/A's - Will consider something else in the future
- rb.gm.data[!is.na(rb.gm.data$DefendersInTheBox),]
+ rb.gm.data <- rb.gm.data[!is.na(rb.gm.data$DefendersInTheBox),]
  
  rb.gm.data <- rb.gm.data[,-c(15,16)]
  
- agg.data.rb <- unique(rb.gm.data) %>%
-                      group_by(NflId,GameId,Quarter,Down) %>%
-                           mutate(firstHalf = if(Quarter<=2){as.numeric(1.0)}else{as.numeric(0)},
-                                  playLeft = if(PlayDirection=="left"){as.numeric(1.0)}else{as.numeric(0)},
-                                  firstDownRun = if(Distance>Yards){as.numeric(1.0)}else{as.numeric(0)},
-                                  grassField = if(grepl("rass",Turf)){as.numeric(1.0)}else{as.numeric(0)},
-                                  goodRun = if(Yards > 5){as.numeric(1)}else{as.numeric(0)})%>%
-                           transmute(avgFieldType = mean(grassField),
-                                     avgFirstHalf = mean(firstHalf),
-                                     avgPlayLeft = mean(playLeft),
-                                     avgFirstDownRun = mean(firstDownRun),
-                                     avgDistancePerDefenders = Distance/DefendersInTheBox,
-                                     avgYardsPerDefenders = Yards/DefendersInTheBox,
-                                     totalYards = sum(Yards), 
-                                     avgYards = mean(Yards), 
-                                     avgHandOfTime = mean(handoffTime), 
-                                     avgDistance=mean(Distance),
-                                     avgDefendersInTheBox = mean(DefendersInTheBox),
-                                     goodRun = goodRun)
-  
-## want to group data by downs so each RB will have 4 entries
-corrplot(cor(agg.data.rb[,c(-1,-2)],method = "spearman",use="pairwise.complete.obs") )
+ #rm(data,nfl.data)
  
+ agg.data.rb <- unique(rb.gm.data) %>%
+                           group_by(NflId,GameId,PlayId, Quarter,Down) %>%
+                           transmute(avgDistancePerDefenders = Distance/DefendersInTheBox,
+                                     avgYardsPerDefenders = Yards/DefendersInTheBox,
+                                     prePlayYards = prePlayYards, 
+                                     cumYards = cumYards,
+                                     avgYards = mean(Yards), 
+                                     handoffTime = handoffTime, 
+                                     Distance = Distance,
+                                     avgDistance=mean(Distance),
+                                     DefendersInTheBox = DefendersInTheBox,
+                                     avgDefendersInTheBox = mean(DefendersInTheBox),
+                                     Yards=Yards,
+                                     goodRun = ifelse(Yards > 5,as.numeric(1),as.numeric(0)))
+ 
+ agg.data.rb$prePlayYards[is.na(agg.data.rb$prePlayYards)] <- 0
+ 
+ ## graphs
+ 
+ g <- ggplot(data=agg.data.rb) 
+ 
+ # plots
+ 
+ g + geom_boxplot(aes(round(avgDefendersInTheBox),avgYards)) + facet_grid(.~Down)
+ g + geom_density(aes(avgYards)) + facet_grid(Quarter~Down)
+ g + geom_jitter(aes(y=(avgYards),x=avgDefendersInTheBox))+facet_grid(Quarter~Down)
+ 
+ g + geom_boxplot(aes(y=Distance,x=factor(Down)))
+ g + geom_boxplot(aes(y=DefendersInTheBox,x=factor(Down)))
+ g + geom_boxplot(aes(y=handoffTime,x=factor(Down)))
+ g + geom_jitter(aes(y=prePlayYards,x=DefendersInTheBox))
+ g + geom_jitter(aes(y=handoffTime,x=DefendersInTheBox))
+ 
+ g + geom_boxplot(aes(y=DefendersInTheBox, x=factor(goodRun)))
+ g + geom_boxplot(aes(y=DefendersInTheBox, x=factor(Quarter)))
+ g + geom_boxplot(aes(y=DefendersInTheBox, x=factor(Down)))
+ 
+ 
+## want to group data by downs so each RB will have 4 entries
+ png(file="corr.png", res=300, width=4500, height=4500)
+ corr <- cor(agg.data.rb[,c(-1,-2,-3)],method = "spearman",use="pairwise.complete.obs")
+ corrplot(corr,method="color" ,addCoef.col="black",number.digits=2,number.cex = 1)
+ dev.off()
 
 
+ ##  Need to Split the data up
+ 
+ train.index <- caret::createDataPartition(agg.data.rb$goodRun,p=.7,list=FALSE)
+ train <- agg.data.rb[train.index,]
+ test <- agg.data.rb[-train.index,]
+ 
 ### working on everything below this comment
 
-g <- ggplot(data=agg.data.rb) 
  
-# plots
-g + geom_boxplot(aes(round(avgDefendersInTheBox),avgYards)) + facet_grid(.~Down)
-g + geom_density(aes(avgYards), bins=11) + facet_grid(.~PlayDirection)
+ ## linear model
 
+ linear.model <- lm(Yards~DefendersInTheBox+Distance+cumYards, data=train)
+ pred <- predict(linear.model,test)
 
-g + geom_line(aes(y=(avgYards),x=avgDefendersInTheBox))+facet_grid(Quarter~Down)
-
-
-
+ actual_preds <-data.frame(cbind(actuals=test$Yards,predicted=pred))
+ 
+ min_max_accuracy <- mean(apply(actual_preds, 1, min) / apply(actual_preds, 1, max)) 
+ 
+ 
